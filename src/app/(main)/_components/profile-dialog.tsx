27ed17f6ea3e349599e3884c22a9cc8892f894d8
yusukegunji <inochideutae@gmail.com>
2024-05-07 02:30:05 +0900

    feat: profile-dialog追加

---
+++ src/app/(main)/_components/profile-dialog.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button";
+import {
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogContent,
+  Dialog,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+export default function ProfileDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default">プロフィールを編集</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            ここでプロフィールを変更します。完了したら保存をクリックしてください
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Name
+            </Label>
+            <Input
+              className="col-span-3"
+              defaultValue="Pedro Duarte"
+              id="name"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="username">
+              Username
+            </Label>
+            <Input
+              className="col-span-3"
+              defaultValue="@peduarte"
+              id="username"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
