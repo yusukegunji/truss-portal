807f2da39e2826a150e7766d2fed1adbfd854c7c
yusukegunji <inochideutae@gmail.com>
2024-05-09 03:40:42 +0900

    feat: カテゴリー追加

---
+++ src/assets/data/category.ts
@@ -0,0 +1,11 @@
+export const Categories = [
+  { name: "ホーム", icon: "home", path: "/" },
+  { name: "Info", icon: "info", path: "info" },
+  { name: "リソース", icon: "file-text", path: "resource" },
+  { name: "リリース情報", icon: "rocket", path: "release" },
+  { name: "メンバー", icon: "users", path: "member" },
+  { name: "プロジェクト", icon: "gantt-chart", path: "project" },
+  { name: "開発", icon: "code", path: "tech" },
+  { name: "API", icon: "globe", path: "api-docs" },
+  { name: "ブログ", icon: "rss", path: "blog" },
+];
